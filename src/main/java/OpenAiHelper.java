9db76f7d8b40e273d63ed692c09757e0d54ccc0e
robaldwin9 <robaldwin9@gmail.com>
2023-01-31 20:12:50 -0500

    Add initial code for image requests
     - Add image request to helper
     - Add moderation checks on all open AI queries

--- src/main/java/OpenAiHelper.java
+++ src/main/java/OpenAiHelper.java
@@ -1,6 +1,11 @@
 import com.theokanning.openai.OpenAiService;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.image.CreateImageRequest;
+import com.theokanning.openai.image.ImageResult;
+import com.theokanning.openai.moderation.Moderation;
+import com.theokanning.openai.moderation.ModerationRequest;
+import com.theokanning.openai.moderation.ModerationResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,6 +26,8 @@ public class OpenAiHelper {
         return instance == null ? (instance = new OpenAiHelper()) : instance;
     }
 
+    private static final String POLICY_VIOLATION_TEXT = "Sorry request does not comply with OpenAI's Content Policy";
+
     private OpenAiHelper() {
         service = new OpenAiService(config.getGptToken());
     }
@@ -31,18 +38,56 @@ public class OpenAiHelper {
      * @return openAi response
      */
     public String makeOpenAiCompletionRequest(String request) {
-        String response = "";
-        var completionRequest = CompletionRequest.builder()
-                .prompt(request)
-                .model(config.getOpenAiModel())
-                .echo(true)
-                .temperature(config.getOpenAiTemperature())
-                .maxTokens(config.getOpenAiMaxTokens())
+        String response = POLICY_VIOLATION_TEXT;
+
+        if(requestMatchesContentPolicy(request)) {
+            var completionRequest = CompletionRequest.builder()
+                    .prompt(request)
+                    .model(config.getOpenAiModel())
+                    .echo(true)
+                    .temperature(config.getOpenAiTemperature())
+                    .maxTokens(config.getOpenAiMaxTokens())
+                    .build();
+
+            CompletionResult completionResult = service.createCompletion(completionRequest);
+            response = completionResult.getChoices().get(0).getText().replaceFirst(request + "\n", "");
+        }
+
+        return response;
+    }
+
+    public String makeOpenAiImageRequest(String request) {
+        String result = POLICY_VIOLATION_TEXT;
+
+        if(requestMatchesContentPolicy(request)) {
+            CreateImageRequest imageRequest = CreateImageRequest.builder()
+                    .prompt(request)
+                    .size("1024x1024")
+                    .responseFormat("url")
+                    .build();
+            ImageResult imageResult = service.createImage(imageRequest);
+            result = imageResult.getData().get(0).getUrl();
+        }
+
+        return result;
+    }
+
+    public boolean requestMatchesContentPolicy(String request) {
+        boolean requestOk = true;
+        ModerationRequest moderationRequest = com.theokanning.openai.moderation.ModerationRequest.builder()
+                .input(request)
                 .build();
 
-        CompletionResult result = service.createCompletion(completionRequest);
-        response = result.getChoices().get(0).getText().replaceFirst(request + "\n", "");
-        return response;
+        ModerationResult result = service.createModeration(moderationRequest);
+        for(Moderation moderation: result.getResults()) {
+            if (moderation.flagged) {
+                logger.debug("moderation check flagged: {}", moderation.toString());
+                requestOk = false;
+                break;
+            }
+        }
+
+        return requestOk;
     }
 
     /**
